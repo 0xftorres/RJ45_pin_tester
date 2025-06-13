e5fd9b7bc87e8c1c4a77f8df8c69d7586a534736
Federico Torres <federicoivantorres1@gmail.com>
2025-06-13 16:45:25 -0300

    refactor code

--- RJ45Checker.ino
+++ RJ45Checker.ino
@@ -24,11 +24,13 @@
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 // Array de pines para c/ conector
-const int C1[] = {PinC1_1, PinC1_2, PinC1_3, PinC1_4, PinC1_5, PinC1_6, PinC1_7, PinC1_8};
-const int C2[] = {PinC2_1, PinC2_2, PinC2_3, PinC2_4, PinC2_5, PinC2_6, PinC2_7, PinC2_8};
+// Output
+const int CO[] = {PinC1_1, PinC1_2, PinC1_3, PinC1_4, PinC1_5, PinC1_6, PinC1_7, PinC1_8};
+// Input
+const int CI[] = {PinC2_1, PinC2_2, PinC2_3, PinC2_4, PinC2_5, PinC2_6, PinC2_7, PinC2_8};
 
-// Matriz de conecciones para StraightThrough
-const int ST[][3] = {
+// Connections matrix for StraightThrough
+const int StraightThrough[][3] = {
   {PinC1_1, PinC2_1, 0},
   {PinC1_2, PinC2_2, 0},
   {PinC1_3, PinC2_3, 0},
@@ -39,8 +41,8 @@ const int ST[][3] = {
   {PinC1_8, PinC2_8, 0}
 };
 
-// Matriz de conecciones para CrossOver
-const int CO[][3] = {
+// Connections matrix for CrossOver
+const int CrossOver[][3] = {
   {PinC1_1, PinC2_1, 0},
   {PinC1_2, PinC2_2, 0},
   {PinC1_3, PinC2_6, 0},
@@ -51,12 +53,77 @@ const int CO[][3] = {
   {PinC1_8, PinC2_8, 0}
 };
 
-void compararPines(int S[][3]){
-  setearTodoBajo(C1);
+void setBitIO(int pin, int state){
+  digitalWrite(pin, state);
+  delay(10);
+}
+
+void setAllLow(int con[]){
+  for(int i=0; i < 8; i++){
+    setBitIO(con[i], LOW);
+    delay(10);
+  }
+}
+
+void setAllHigh(int con[]){
+  for(int i=0; i < 8; i++){
+    setBitIO(con[i], HIGH);
+    delay(10);
+  }
+}
+
+void setAllInput(int con[]){
+  for(int i=0; i < 8; i++){
+    pinMode(con[i], INPUT);
+    delay(10);
+  }
+}
+
+void setAllOutput(int con[]){
+  for(int i=0; i < 8; i++){
+    pinMode(con[i], OUTPUT);
+    delay(10);
+  }
+}
+
+void cableNotDetectedScreen(){
+  lcd.clear();
+  lcd.print("Conectar cables RJ45");
+
+   for (int i=0; i < 20; i++) {
+    lcd.setCursor(i,2);
+    if(i%2==0) {
+      lcd.print("-");
+    } else{
+      lcd.print("_");
+    }
+    delay(200);
+  }
+}
+
+bool detectCable(){
+  setAllHigh(CO);
+  for(int i=0; i < 8; i++){
+    if (digitalRead(CI[i]) == HIGH){
+      return true;
+    }
+    delay(10);
+  }
+  return false;
+}
+
+void comparePins(int S[][3]){
+  // set every output pin to low
+  setAllLow(CO);
+
+  // loop parameter
   for (int i=0; i < 8; i++){
-    setearIO(S[i][0], HIGH);
+
+    // set pin to 1
+    setBitIO(S[i][0], HIGH);
+
     if (digitalRead(S[i][1]) == HIGH){
-      // Esta bien conectado
+      // link is correct
       Serial.print(S[i][0]);
       Serial.print("-->");
       Serial.println(S[i][1]);
@@ -64,105 +131,62 @@ void compararPines(int S[][3]){
       lcd.print("| ");
       delay(50);
     } else{
-      // No esta bien conectado
+      // link is incorrect
       lcd.print("X ");
       delay(50);
     }
-    setearIO(S[i][0], LOW);
-    delay(10);
+
+    // set pin back to 0
+    setBitIO(S[i][0], LOW);
   }
-}
-
-void setearIO(int pin, int state){
-  digitalWrite(pin, state);
-  delay(10);
-}
-
-void setearTodoBajo(int con[]){
-  for(int i=0; i < 8; i++){
-    setearIO(con[0], LOW);
-  }
-}
-
-void setearTodoAlto(int con[]){
-  for(int i=0; i < 8; i++){
-    setearIO(con[i], HIGH);
-    delay(10);
-  }
-}
-
-void setearTodosEntrada(int con[]){
-  for(int i=0; i < 8; i++){
-    pinMode(con[i], INPUT);
-  }
-}
-
-void setearTodosSalida(int con[]){
-  for(int i=0; i < 8; i++){
-    pinMode(con[i], OUTPUT);
-  }
-}
-
-bool detectarCables(){
-  setearTodoAlto(C1);
-  for(int i=0; i < 8; i++){
-    if (digitalRead(C2[i]) == HIGH){
-      return true;
-    }
-  }
-  return false;
-}
-
-void inicializar(){
-  setearTodosSalida(C1);
-  setearTodosEntrada(C2);
 }
 
 void setup()
 {
+  // serial for debugging
   Serial.begin(9600);
 
+  // lcd setup
   lcd.init();
   lcd.backlight();
-  inicializar();
+
+  // initial state for pins
+  setAllOutput(CO);
+  setAllInput(CI);
 }
 
 void loop()
 {
-  if(detectarCables()){
+  if(detectCable()){
 
     lcd.clear();
+
+    // print numbers
     lcd.setCursor(4,0);
     lcd.print("1 2 3 4 5 6 7 8");
     lcd.setCursor(4,3);
     lcd.print("1 2 3 4 5 6 7 8");
 
-
+    // print labels
     lcd.setCursor(1,1);
     lcd.print("ST ");
     lcd.setCursor(1,2);
     lcd.print("CO ");
 
+    // print straight-through
     lcd.setCursor(4,1);
-    compararPines(ST);
+    comparePins(StraightThrough);
 
+    // print straight-through
     lcd.setCursor(4,2);
-    compararPines(CO);
-    //lcd.print("CO | | | | | | | X");
-    delay(10000);
-  } else {
-    setearTodoBajo(C1);
-    lcd.clear();
-    lcd.print("Conectar cables RJ45");
+    comparePins(CrossOver);
 
-    for (int i=0; i < 20; i++){
-      lcd.setCursor(i,2);
-      if(i%2==0){
-        lcd.print("-");
-      }else{
-        lcd.print("_");
-      }
-      delay(200);
-    }
+    delay(10000);
+  }
+  
+  else {
+    setAllLow(CO);
+    cableNotDetectedScreen();
   }
 }
+
